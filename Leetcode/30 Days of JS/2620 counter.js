afaa60b60f108eff22c2853511009c27f24cdc3e
James Roche <124353082+JRBOB123@users.noreply.github.com>
2024-01-27 00:58:46 +1100

    Create 2620 counter.js
    Added code of 2nd 30 days of JS challenge

---
+++ Leetcode/30 Days of JS/2620 counter.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let currentCount = n - 1;
+    return function() {
+        currentCount += 1;
+        return currentCount;
+    };
+};
